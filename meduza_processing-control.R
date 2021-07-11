e2c0fc9d8b2e74d3ab60a33c7a0afedd339a8897
nikolkj <nikolaipriser@gmail.com>
2021-07-11 14:04:06 -0500

    print queue length to console.

--- meduza_processing-control.R
+++ meduza_processing-control.R
@@ -1,10 +1,12 @@
 source(file = "meduza_check-home.R") # check home page, update queue
 
 check_queue = readRDS(file = "meduza-processing-queue.rds") # get updated queue
+# View(check_queue)
 check_queue = check_queue$processed
 
+
 while(FALSE %in% check_queue){
-  
+  print(paste("Files to process:", sum(!check_queue))) # print queue length to console
   source(file = "meduza_synthesize-article.R") # process article
   check_queue = readRDS(file = "meduza-processing-queue.rds") # get updated queue
   check_queue = check_queue$processed
